2ec1665aff0556b0c632d91bab5f0a0bbd606e4b
Jonatan Rodrigues da Silva <jonatanjrss@gmail.com>
2025-03-30 20:00:22 -0300

    build to android

--- exemplo/__main__.py
+++ exemplo/__main__.py
@@ -1,12 +1,42 @@
-from cyclopts import App
-from rich import print
-
-app = App()
+import toga
 
 
-@app.default
-def olar(nome: str):
-    print(f'Olar {nome}!')
+def button_handler(widget):
+    print("hello")
 
 
-app()
+def build(app):
+    box = toga.Box()
+
+    button = toga.Button("Hello world", on_press=button_handler)
+    button.style.margin = 50
+    button.style.flex = 1
+    box.add(button)
+
+    return box
+
+
+def main():
+    return toga.App("First App", "org.beeware.toga.examples.tutorial", startup=build)
+
+
+if __name__ == "__main__":
+    main().main_loop()
+
+
+
+
+
+
+# from cyclopts import App
+# from rich import print
+
+# app = App()
+
+
+# @app.default
+# def olar(nome: str):
+#     print(f'Olar {nome}!')
+
+
+# app()
